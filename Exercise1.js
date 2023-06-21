1a23aab306390a4a82e6836686758c1e7c194866
akshaypraf1994 <aks.patil18@gmail.com>
2023-06-21 18:08:08 +0530

    js exercise 1 is completed

---
+++ Exercise1.js
@@ -0,0 +1,32 @@
+
+//if and else problem
+/*
+let age= prompt("what is your age");
+if(age>10 && age<20){
+    console.log("your age bet 10 and 20")
+}
+else{
+    console.log("your age not bet 10 and 20")
+}*/
+
+
+//switch case problem
+
+let age= prompt("what is your age");
+switch(age){
+    case '12' :
+    console.log("age is 12");
+    break
+    case '22':
+    console.log("age is 22")
+    break
+    case '33':
+    console.log("age is 33")
+    break
+    case '44':
+    console.log("is 44")
+    break
+    default:
+    console.log("invalid")
+    
+}
