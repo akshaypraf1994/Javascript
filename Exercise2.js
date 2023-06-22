b07ed94254be514b47cae6630e76c65aa9a57c29
akshaypraf1994 <aks.patil18@gmail.com>
2023-06-22 11:22:51 +0530

    Execise2 is completed

---
+++ Exercise2.js
@@ -0,0 +1,42 @@
+
+
+//for loop
+
+for (i=0;i<500;i++){
+    console.log(i+1)
+}
+
+
+//for in loop & for of loop
+
+let obj= {
+harry:"78",
+akshay:"99",
+mukesh:"88"
+}
+
+for(let a in obj){
+ console.log("marks of " +a + "are" + obj[a])
+}
+
+
+for(let b of "akshay"){
+    console.log(b)
+   }
+
+// while loop
+
+i=0;
+while(i<5){
+ 
+   console.log(i)
+   i++;
+}
+
+// do while loop
+
+i=0
+do{
+    console.log(i)
+    i++;
+}while(i<6)
