4df51f0177f03793416353a38af415380609cac5
akshaypraf1994 <aks.patil18@gmail.com>
2023-06-22 14:05:59 +0530

    Execise2 is completed

---
+++ Exercise3.js
@@ -0,0 +1,22 @@
+
+// arrow function 
+const hello =()=>{
+    console.log("how are you")
+
+}
+
+hello();
+
+// function
+
+function avgplus(x ,y){
+    console.log("done")
+return (x+y)/2
+
+}
+
+a= 1;
+b= 2;
+c= 3;
+
+console.log("addition and avg", avgplus(a,b) )
---
+++ Exercise4.js
@@ -0,0 +1,18 @@
+//string ex 1
+
+let string= "akshay"
+
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+console.log(string.slice(2))
+
+
+
+//string ex 2
+
+
+let str2= "my name is akshay"
+
+let amount= str2.slice("my name is".length)
+
+console.log(amount)
